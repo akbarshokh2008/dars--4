0b4c2863c8ea6b3c9ad4ef7f784e41215ac07bd6
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-05-31 22:28:22 +0500

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>Barcha huquqlar himoyalangan.</p>
+        <a href="https://chatgpt.com/" target="_blank">
+          Akbarjon Mo'sinov
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
